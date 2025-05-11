41b3a6c105ce9177997111ddb181299fcc72e8c3
Dmytro  Shynkarenko <d.o.shynkarenko@gmail.com>
2025-05-11 14:07:08 +0300

    Recovering from errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
 type Props = {
   error: Error;
+  reset: () => void;
 };
 
-const ErrorBoundary = ({ error }: Props) => {
-  return <div>{error.message}</div>;
+const ErrorBoundary = ({ error, reset }: Props) => {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={reload}>Reset</button>
+    </div>
+  );
 };
 
 export default ErrorBoundary;
