bf76908db7ef418dfa6ee299e768863dad12be99
Dmytro  Shynkarenko <d.o.shynkarenko@gmail.com>
2025-05-29 22:01:23 +0300

    Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
   // const requestHeaders = new Headers(request.headers);
@@ -8,7 +8,17 @@ export async function GET(request: NextRequest) {
   const headerList = await headers();
   console.log(headerList.get("Authorization"));
 
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "20");
+  console.log(cookieStore.get("resultsPerPage"));
+
   return new Response("<h1>Profile Data!</h1>", {
-    headers: { "Content-Type": "text/html" },
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
   });
 }
