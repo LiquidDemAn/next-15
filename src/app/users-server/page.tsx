10f155401b50f0e216af207d5289de4c73259183
Dmytro  Shynkarenko <d.o.shynkarenko@gmail.com>
2025-06-12 20:48:43 +0300

    Fetching data in Server Components

---
+++ src/app/users-server/page.tsx
@@ -0,0 +1,32 @@
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+};
+
+async function UsersServerPage() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await response.json();
+
+  return (
+    <ul>
+      {users.map(({ id, name, username, email, phone }) => (
+        <li
+          key={id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <div className="font-bold">{name}</div>
+          <div className="text-sm">
+            <div>Username: {username}</div>
+            <div>Email: {email}</div>
+            <div>Phone: {phone}</div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default UsersServerPage;
