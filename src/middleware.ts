e3b652e822cd15d9f8112047bd97414d6c4dc790
Dmytro  Shynkarenko <d.o.shynkarenko@gmail.com>
2025-06-22 17:32:37 +0300

    Based Access Control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 // const isProtectedRoute = createRouteMatcher(["/user-profile"]);
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
@@ -12,8 +15,11 @@ export default clerkMiddleware(async (auth, request) => {
   //   await auth.protect();
   // }
 
-  const { userId, redirectToSignIn } = await auth();
+  const { userId, redirectToSignIn, sessionClaims } = await auth();
 
+  if (isAdminRoute(request) && sessionClaims?.metadata.role !== "admin") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
   if (!userId && !isPublicRoute(request)) {
     return redirectToSignIn();
   }
